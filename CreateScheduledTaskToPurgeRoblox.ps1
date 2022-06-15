7f93757324bf26762bfa11de2e10377c2ab0ad29
Connor Hudson <36829070+nettrojan@users.noreply.github.com>
2022-06-15 07:35:26 -0500

    Add files via upload

---
+++ CreateScheduledTaskToPurgeRoblox.ps1
@@ -0,0 +1,6 @@
+﻿$trigger = New-ScheduledTaskTrigger -Daily -At 8:30am
+$settings = New-ScheduledTaskSettingsSet -Hidden -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '$packagesInstalled = Get-AppxPackage -AllUsers; foreach ($p in $packagesInstalled){if ($p.Name -eq "ROBLOXCORPORATION.ROBLOX"){Get-AppxPackage -AllUsers *ROBLOXCORPORATION.ROBLOX* | Remove-AppPackage;}} ;$loggedInUser = $(Get-CimInstance -ClassName Win32_ComputerSystem | select username).username; if ($loggedInUser.Contains("AzureAD\")){$loggedInUser = $loggedInUser.replace("AzureAD\",""); $dir = "C:\Users\" + $loggedInUser + "\AppData\Local\Roblox"; Get-ChildItem -Directory -Path $dir | Remove-Item -Force -Recurse;}'
+$task = Register-ScheduledTask -TaskName 'Delete Roblox' -Action $action -Trigger $trigger -Settings $settings -Description 'Deletes Roblox in the most annoying way possible >:)' -RunLevel Highest -Force
+
+
---
+++ KillRobloxOnStudentDevices.ps1
@@ -0,0 +1,39 @@
+﻿# 
+# KillRoblox.ps1
+# Connor Hudson, 2022
+# 
+# Turn this into a Scheduled Task for ultimate annoyance
+# 
+$packagesInstalled = Get-AppxPackage -AllUsers
+
+try{
+    # Look for instances of Roblox installed through Microsoft Store, delete them
+    foreach ($p in $packagesInstalled){
+        if ($p.Name -eq "ROBLOXCORPORATION.ROBLOX"){
+            Get-AppxPackage -AllUsers *ROBLOXCORPORATION.ROBLOX* | Remove-AppPackage
+        }
+    }
+} catch {"An error occured, could not locate or delete a version of Roblox downloaded from Microsoft Store."}
+
+
+# Get the current logged in user
+$loggedInUser = $(Get-CimInstance -ClassName Win32_ComputerSystem | select username).username
+
+
+#This step necessary if the device and user are AzureAD joined
+if ($loggedInUser.Contains("AzureAD\")){
+    echo "$loggedInUser is AzureAD Joined!"
+    $loggedInUser = $loggedInUser.replace("AzureAD\","")
+    $dir = "C:\Users\" + $loggedInUser + "\AppData\Local\roblox"
+    
+
+    
+    #echo "C:\users\StudentTestingAccoun\AppData"
+    echo $dir
+    # Find any files accosiated with Roblox, delete them, -whatif the line to see results without deleting anything
+    Get-ChildItem -Directory -Path $dir | Remove-Item -Force -Recurse
+    #Get-ChildItem -directory -path  | where {$_name -like 'Roblox*'} | Remove-Item -Recurse -Force -whatif
+} else {
+    # This line works on devices that were manually joined to a domain
+    Get-ChildItem -directory -Path $env:LOCALAPPDATA | where {$_name -like 'Roblox*'} | Remove-Item -Force -Recurse
+}
